e4f0c4b8185a29f70d1f4e1bd6bef6ca273df9bb
vince <22629136+thermoscookies@users.noreply.github.com>
2021-03-20 15:23:59 -0700

    fix last message

--- src/system_sensors.py
+++ src/system_sensors.py
@@ -372,7 +372,7 @@ def send_config_message(mqttClient):
     mqttClient.publish(
         topic=f"homeassistant/sensor/{deviceName}/last_message/config",
         payload='{"device_class":"timestamp",'
-                + f"\"name\":\"{deviceNameDisplay} Last Message\","
+                + f"\"name\":\"Last Update\","
                 + f"\"state_topic\":\"system-sensors/sensor/{deviceName}/state\","
                 + '"value_template":"{{value_json.last_message}}",'
                 + f"\"unique_id\":\"{deviceName}_sensor_last_message\","
